0ff3466643169dccf88967cc3397a5fd328799cd
nkaeming <niklas.kaeming@yahoo.com>
2016-12-16 20:43:12 +0100

    Das System wurde migriert in eine Umgebung die nun zu 99% Objektorintiert ist. Einzige Ausnahme bilden nur die IOHelper, welche die Config und Logfiles auslesen. Es fehlt dir Migration des UI.

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from Services.PortService import PortService
 from Services.TriggerService import TriggerService
 
 PS = PortService()
-TS = TriggerService()
+TS = TriggerService(PS)
 PS.setTriggerService(TS)

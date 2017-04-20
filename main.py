acc1c6d0f100e4f82836c8de301eb7ec7c41ab1b
nkaeming <niklas.kaeming@yahoo.com>
2017-04-20 09:54:04 +0200

    Bugfixing.

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from Services.TriggerService import TriggerService
 from Services.AlertService import AlertService
 from UI.UIServer import UIServer
 from statusLED import LEDThread
+import os
 
 if __name__ == '__main__':
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
     AS = AlertService()
     PS = PortService()
     TS = TriggerService(PS, AS)
